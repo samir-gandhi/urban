f3d864039ecba9a5438c2920d4e92f3c95eb3176
samirgandhi <17574913+samir-gandhi@users.noreply.github.com>
2022-07-01 16:03:48 -0600

    Initialize destructuring

---
+++ scripts/prune.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+## THIS IS TO CLEAN THE RELEASE RELATED TO YOUR CURRENT BRANCH IN GIT.
+## *****IT CAN BE VERY DESTRUCTIVE*****
+## Be sure you're on the right git branch. 
+
+CWD=$(dirname "$0")
+. "${CWD}/vars.sh"
+. "${CWD}/functions.sh"
+getLocalSecrets
+getEnv
+getNamespace
+
+helm uninstall "${ENV}" -n "${K8S_NAMESPACE}"
+if test "${1}" = "--heavy" ; then
+  kubectl delete pvc --selector=app.kubernetes.io/instance="${ENV}"
+  kubectl delete ns "${K8S_NAMESPACE}"
+fi
+
+echo "${GREEN} INFO: Environment pruned - ${ENV} ${NC}"
